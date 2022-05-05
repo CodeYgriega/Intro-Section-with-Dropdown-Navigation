861af3fc85f7f809758fad4ed9440a31124a3095
CodeYgriega <oliveryanicelli99@gmail.com>
2022-05-05 20:45:39 -0300

    Desafio-Completo-Con-Responsive

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,20 @@
 const hamburguesa = document.getElementById("menu-hamburguesa");
 const hamburguesa_desplegado = document.getElementById("hamburguesa-desplegado");
 const close = document.getElementById("close-hamburguesa");
+const body = document.getElementById("body");
 
 hamburguesa.addEventListener("click", (event) => {
     event.preventDefault();
     hamburguesa_desplegado.classList.toggle("hamburguesa-desplegado-hidden");
     hamburguesa.classList.toggle("menu-hamburguesa-hidden");
+    body.classList.toggle("body");
 })
 
 close.addEventListener("click", (event) => {
     event.preventDefault();
     hamburguesa_desplegado.classList.toggle("hamburguesa-desplegado-hidden");
     hamburguesa.classList.toggle("menu-hamburguesa-hidden");
+    body.classList.toggle("body");
 })
 
 //ahora empezamos a hacer lo mismo pero con las secciones de el menu desplegable
@@ -40,4 +43,33 @@ company.addEventListener("click", (event) => {
     menu_company.classList.toggle("menu-company-hidden");
     flecha_abajo_company.classList.toggle("flecha-abajo-company-hidden");
     flecha_arriba_company.classList.toggle("flecha-arriba-company-hidden");
+})
+
+//hacemos lo mismo para la versión de pc 
+
+const features_desktop = document.getElementById("features_desktop");
+const menu_features_desktop = document.getElementById("menu_features_desktop");
+const flecha_abajo_features_desktop = document.getElementById("flecha_abajo_features_desktop");
+const flecha_arriba_features_desktop = document.getElementById("flecha_arriba_features_desktop");
+
+features_desktop.addEventListener("click", (event) => {
+    event.preventDefault();
+    menu_features_desktop.classList.toggle("menu-features-hidden");
+    menu_features_desktop.classList.toggle("features-desplegado");
+    flecha_abajo_features_desktop.classList.toggle("flecha-abajo-features-hidden");
+    flecha_arriba_features_desktop.classList.toggle("flecha-arriba-features-hidden");
+})
+
+
+const company_desktop = document.getElementById("company_desktop");
+const menu_company_desktop = document.getElementById("menu_company_desktop");
+const flecha_abajo_company_desktop = document.getElementById("flecha_abajo_company_desktop");
+const flecha_arriba_company_desktop = document.getElementById("flecha_arriba_company_desktop");
+
+company_desktop.addEventListener("click", (event) => {
+    event.preventDefault();
+    menu_company_desktop.classList.toggle("menu-company-hidden");
+    menu_company_desktop.classList.toggle("company-desplegado");
+    flecha_abajo_company_desktop.classList.toggle("flecha-abajo-company-hidden");
+    flecha_arriba_company_desktop.classList.toggle("flecha-arriba-company-hidden");
 })
